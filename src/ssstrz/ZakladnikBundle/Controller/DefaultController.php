02169f5a013f6912c1a67e3c45d84f3ccad452d1
ssstrz <codecats@gmail.com>
2014-02-16 12:07:28 +0100

    js added

--- src/ssstrz/ZakladnikBundle/Controller/DefaultController.php
+++ src/ssstrz/ZakladnikBundle/Controller/DefaultController.php
@@ -3,11 +3,13 @@
 namespace ssstrz\ZakladnikBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction($name, Request $request)
     {
+     //   $request->setLocale('p');
         return $this->render('ssstrzZakladnikBundle:Default:index.html.twig', array('name' => $name));
     }
 }
