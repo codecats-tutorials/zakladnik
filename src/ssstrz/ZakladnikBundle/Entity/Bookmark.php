c85fa9b054b42324735fe2ce2bcdc07bec273317
ssstrz <codecats@gmail.com>
2014-02-17 17:37:44 +0100

    user entity

---
+++ src/ssstrz/ZakladnikBundle/Entity/Bookmark.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace ssstrz\ZakladnikBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Bookmark
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="ssstrz\ZakladnikBundle\Entity\BookmarkRepository")
+ */
+class Bookmark
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Bookmark
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return Bookmark
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string 
+     */
+    public function getUrl()
+    {
+        return $this->url;
+    }
+}
